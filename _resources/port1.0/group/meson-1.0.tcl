62f022f8efe17b1e09462a52d27d0432450e1527
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2023-01-26 00:23:03 -0500

    meson-1.0: move setup to configure.pre_args

--- _resources/port1.0/group/meson-1.0.tcl
+++ _resources/port1.0/group/meson-1.0.tcl
@@ -25,7 +25,8 @@ depends_skip_archcheck-append \
 
 # TODO: --buildtype=plain tells Meson not to add its own flags to the command line. This gives the packager total control on used flags.
 default configure.cmd       {${prefix}/bin/meson}
-default configure.post_args {setup [meson::get_post_args]}
+default configure.pre_args  {setup}
+default configure.post_args {[meson::get_post_args]}
 configure.universal_args-delete \
                             --disable-dependency-tracking
 
